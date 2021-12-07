03b25ecded86b21bd8e575882c5dba7b8621cc84
oddgrd <oddgrd@gmail.com>
2021-12-07 21:19:57 +0100

    fixed with triangular number formula

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,40 +1,31 @@
 use std::fs;
 
-fn parse_input(input: &String) -> Vec<u32> {
-    let mut sorted: Vec<u32> = input.split(",").map(|n| n.parse().unwrap()).collect();
+fn parse_input(input: &String) -> Vec<i32> {
+    let mut sorted: Vec<i32> = input.split(",").map(|n| n.parse().unwrap()).collect();
     sorted.sort();
     sorted
 }
 
-fn part_one(positions: Vec<u32>) -> u32 {
+fn part_one(positions: Vec<i32>) -> i32 {
     let mid = positions[positions.len() / 2];
 
-    positions.iter().fold(0, |mut fuel_cost: u32, crab| {
-        if crab > &mid {
-            fuel_cost += crab - mid;
-        } else {
-            fuel_cost += mid - crab;
-        }
+    positions.iter().fold(0, |mut fuel_cost: i32, crab| {
+        fuel_cost += (mid - crab).abs();
         fuel_cost
     })
 }
 
-fn sum_range(end: u32) -> u32 {
-    (0..=end).collect::<Vec<u32>>().iter().sum()
+fn triangle(n: i32) -> i32 {
+    (n * (n + 1)) / 2
 }
-fn calc_fuel_cost(positions: &Vec<u32>, mid: u32) -> u32 {
-    positions.iter().fold(0, |mut fuel_cost: u32, crab| {
-        if crab > &mid {
-            fuel_cost += sum_range(crab - mid);
-        } else {
-            fuel_cost += sum_range(mid - crab);
-        }
+fn calc_fuel_cost(positions: &Vec<i32>, mid: i32) -> i32 {
+    positions.iter().fold(0, |mut fuel_cost: i32, crab| {
+        fuel_cost += triangle((mid - crab).abs());
         fuel_cost
     })
 }
-
-fn part_two(positions: Vec<u32>) -> u32 {
-    let mid: u32 = positions[positions.len() / 2];
+fn part_two(positions: Vec<i32>) -> i32 {
+    let mid: i32 = positions[positions.len() / 2];
     let mut fuel_cost = calc_fuel_cost(&positions, mid);
     let mut i = 0;
     loop {
@@ -50,11 +41,4 @@ fn main() {
     let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
     println!("First winner: {:?}", part_one(parse_input(&contents)));
     println!("Second winner: {:?}", part_two(parse_input(&contents)));
-    // println!(
-    //     "Last winner: {:?}",
-    //     bingo_last_winner(
-    //         &parse_input(&contents).1,
-    //         parse_boards(parse_input(&contents).0)
-    //     )
-    // );
 }
