d79d6748974465894aaf524a5309957fe1539df4
oddgrd <oddgrd@gmail.com>
2022-05-28 13:12:02 +0200

    Refactor

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,41 +1,46 @@
 use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+
+    let now = Instant::now();
+    let parsed_input = parse_input(&contents);
+
+    let part_one = count_increases(&parsed_input);
+    let part_two = count_increases_triplets(&parsed_input);
+    let time = now.elapsed().as_micros();
+
+    println!(
+        "Part one: {}\nPart two: {}\nTime: {} μs",
+        part_one, part_two, time
+    );
+}
 
 fn parse_input(input: &str) -> Vec<u32> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
-fn count_increases(input: Vec<u32>) -> u32 {
+fn count_increases(input: &[u32]) -> u32 {
     let mut counter = 0;
-    let mut prev = input[0];
-    for num in input {
-        if num > prev {
+
+    for i in 1..input.len() {
+        if input[i] > input[i - 1] {
             counter += 1;
         }
-        prev = num;
     }
     counter
 }
 
-fn count_increases_triplets(input: Vec<u32>) -> u32 {
+/// The last two numbers of the first triple and the first two of the next triplet overlap
+/// Only compare the non-overlapping numbers
+fn count_increases_triplets(input: &[u32]) -> u32 {
     let mut counter = 0;
-    let mut prev: u32 = input[0..=2].iter().sum();
-    for i in 0..input.len() - 2 {
-        let sum = (input[i..=i + 2]).iter().sum();
-        if sum > prev {
+
+    for i in 3..input.len() {
+        if input[i] > input[i - 3] {
             counter += 1;
         }
-        prev = sum;
     }
     counter
-}
-fn main() {
-    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    println!(
-        "Single increases count:\n{}",
-        count_increases(parse_input(&contents))
-    );
-    println!(
-        "Sum triplets increases count:\n{}",
-        count_increases_triplets(parse_input(&contents))
-    );
 }
--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,57 +1,76 @@
 use std::fs;
+use std::time::Instant;
 
-fn parse_input(contents: &str) -> Vec<(&str, i32)> {
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    let now = Instant::now();
+    let parsed_input = parse_input(&contents);
+
+    let part_one = final_position(&parsed_input);
+    let part_two = final_position_with_aim(&parsed_input);
+    let time = now.elapsed().as_micros();
+
+    println!(
+        "Part one: {}\nPart two: {}\nTime: {} μs",
+        part_one, part_two, time
+    );
+}
+
+fn parse_input(contents: &str) -> Vec<Direction> {
     contents
         .lines()
-        .map(|line| {
-            (
-                &line[0..line.len() - 2],
-                (&line[line.len() - 1..]).parse().unwrap(),
-            )
-        })
+        .map(|line| line.try_into().expect("Invalid input"))
         .collect()
 }
 
-fn final_position(input: Vec<(&str, i32)>) -> i32 {
+enum Direction {
+    Forward(u32),
+    Down(u32),
+    Up(u32),
+}
+impl TryFrom<&str> for Direction {
+    type Error = ();
+
+    fn try_from(line: &str) -> Result<Self, Self::Error> {
+        let distance = (&line[line.len() - 1..]).parse().expect("Not an integer");
+
+        match &line[0..line.len() - 2] {
+            "forward" => Ok(Direction::Forward(distance)),
+            "down" => Ok(Direction::Down(distance)),
+            "up" => Ok(Direction::Up(distance)),
+            _ => Err(()),
+        }
+    }
+}
+
+fn final_position(input: &[Direction]) -> u32 {
     let mut horizontal = 0;
     let mut depth = 0;
 
-    for (a, b) in input {
-        match a {
-            "forward" => horizontal += b,
-            "down" => depth += b,
-            "up" => depth -= b,
-            _ => (),
+    for direction in input {
+        match direction {
+            Direction::Forward(dist) => horizontal += dist,
+            Direction::Down(dist) => depth += dist,
+            Direction::Up(dist) => depth -= dist,
         }
     }
     horizontal * depth
 }
 
-fn final_position_two(input: Vec<(&str, i32)>) -> i32 {
+fn final_position_with_aim(input: &[Direction]) -> u32 {
     let mut aim = 0;
     let mut horizontal = 0;
     let mut depth = 0;
 
-    for (a, b) in input {
-        match a {
-            "forward" => {
-                horizontal += b;
-                depth += b * aim;
+    for direction in input {
+        match direction {
+            Direction::Forward(dist) => {
+                horizontal += dist;
+                depth += dist * aim;
             }
-            "down" => aim += b,
-            "up" => aim -= b,
-            _ => (),
+            Direction::Down(dist) => aim += dist,
+            Direction::Up(dist) => aim -= dist,
         }
     }
     horizontal * depth
-}
-
-fn main() {
-    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-
-    println!("Final position {}", final_position(parse_input(&contents)));
-    println!(
-        "Final position with aim {}",
-        final_position_two(parse_input(&contents))
-    );
 }
