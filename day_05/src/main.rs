9ef9291985f83d596945e12dc3f278538dab573e
oddgrd <oddgrd@gmail.com>
2022-06-05 17:11:09 +0200

    Refactor

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,51 +1,96 @@
-use std::fs;
+use std::{fs, time::Instant};
 
-fn parse_input(contents: &str) -> Vec<Vec<usize>> {
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+
+    let now = Instant::now();
+    let parsed_input = parse_input(&contents);
+
+    let part_one = find_overlaps_one(&parsed_input);
+    let part_two = find_overlaps(&parsed_input);
+    let time = now.elapsed().as_micros(); // 2.2ms
+
+    println!(
+        "Part one: {}\nPart two: {}\nTime: {} μs",
+        part_one, part_two, time
+    );
+}
+
+#[derive(Debug)]
+struct Line {
+    /// (x, y)
+    start: (usize, usize),
+    /// (x, y)
+    end: (usize, usize),
+}
+
+fn parse_input(contents: &str) -> Vec<Line> {
     contents
         .lines()
         .map(|line| line.replace(" -> ", " "))
-        .map(|line| line.replace(",", " "))
+        .map(|line| line.replace(',', " "))
         .fold(Vec::new(), |mut coords, line| {
-            coords.push(line.split(' ').map(|s| s.parse().unwrap()).collect());
+            coords.push({
+                let line: Vec<usize> = line.split(' ').map(|s| s.parse().unwrap()).collect();
+
+                Line {
+                    start: (line[0], line[1]),
+                    end: (line[2], line[3]),
+                }
+            });
             coords
         })
 }
 
-fn sum_overlaps(grid: Vec<Vec<usize>>) -> usize {
-    grid.into_iter().flatten().filter(|n| *n > 1).count()
-}
+fn find_overlaps_one(lines: &[Line]) -> usize {
+    let mut grid = vec![vec![0u8; 1000]; 1000];
 
-fn find_overlaps(coordinates: Vec<Vec<usize>>) -> usize {
-    let mut grid: Vec<Vec<usize>> = vec![vec![0; 1000]; 1000];
-
-    coordinates.iter().for_each(|coord| {
-        if coord[0] == coord[2] {
-            let mut x = vec![coord[1], coord[3]];
-            x.sort_unstable();
-            for i in x[0]..=x[1] {
-                grid[i][coord[0]] += 1;
-            }
-        } else if coord[1] == coord[3] {
-            let mut y = vec![coord[0], coord[2]];
+    lines.iter().for_each(|line| {
+        if line.start.0 == line.end.0 {
+            let mut y = vec![line.start.1, line.end.1];
             y.sort_unstable();
             for i in y[0]..=y[1] {
-                grid[coord[1]][i] += 1;
+                grid[i][line.start.0] += 1;
+            }
+        } else if line.start.1 == line.end.1 {
+            let mut x = vec![line.start.0, line.end.0];
+            x.sort_unstable();
+            for i in x[0]..=x[1] {
+                grid[line.start.1][i] += 1;
+            }
+        }
+    });
+    grid.into_iter()
+        .map(|row| row.into_iter().filter(|&x| x > 1).count())
+        .sum()
+}
+fn find_overlaps(lines: &[Line]) -> usize {
+    let mut grid = vec![vec![0u8; 1000]; 1000];
+
+    lines.iter().for_each(|line| {
+        if line.start.0 == line.end.0 {
+            let mut y = vec![line.start.1, line.end.1];
+            y.sort_unstable();
+            for i in y[0]..=y[1] {
+                grid[i][line.start.0] += 1;
+            }
+        } else if line.start.1 == line.end.1 {
+            let mut x = vec![line.start.0, line.end.0];
+            x.sort_unstable();
+            for i in x[0]..=x[1] {
+                grid[line.start.1][i] += 1;
             }
         } else {
-            let x = vec![coord[0], coord[2]];
-
-            let x: Vec<usize> = if x[0] > x[1] {
-                (x[1]..=x[0]).rev().collect()
+            let x: Vec<usize> = if line.start.0 > line.end.0 {
+                (line.end.0..=line.start.0).rev().collect()
             } else {
-                (x[0]..=x[1]).collect()
+                (line.start.0..=line.end.0).collect()
             };
 
-            let y = vec![coord[1], coord[3]];
-
-            let y: Vec<usize> = if y[0] > y[1] {
-                (y[1]..=y[0]).rev().collect()
+            let y: Vec<usize> = if line.start.1 > line.end.1 {
+                (line.end.1..=line.start.1).rev().collect()
             } else {
-                (y[0]..=y[1]).collect()
+                (line.start.1..=line.end.1).collect()
             };
 
             for i in 0..x.len() {
@@ -53,13 +98,7 @@ fn find_overlaps(coordinates: Vec<Vec<usize>>) -> usize {
             }
         }
     });
-    sum_overlaps(grid)
-}
-
-fn main() {
-    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    println!(
-        "Vertical, horizontal and diagonal overlaps: \n{:?}",
-        find_overlaps(parse_input(&contents))
-    );
+    grid.into_iter()
+        .map(|row| row.into_iter().filter(|&x| x > 1).count())
+        .sum()
 }
--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,38 +1,47 @@
-use std::fs;
+use std::{fs, time::Instant};
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+
+    let now = Instant::now();
+    let parsed_input = parse_input(&contents);
+
+    let part_one = simulate_lanternfish(&parsed_input, 80);
+    let part_two = simulate_lanternfish(&parsed_input, 256);
+    let time = now.elapsed().as_micros(); // 6μs
+
+    println!(
+        "Part one: {}\nPart two: {}\nTime: {} μs",
+        part_one, part_two, time
+    );
+}
 
 fn parse_input(contents: &str) -> Vec<usize> {
     contents.split(',').map(|n| n.parse().unwrap()).collect()
 }
 
-fn simulate_lanternfish(seed: Vec<usize>, days: i32) -> usize {
-    let mut fish: Vec<usize> = vec![0; 9];
+fn simulate_lanternfish(seed: &[usize], days: u16) -> u64 {
+    // Track the number of fish in each state
+    let mut fish = [0u64; 9];
 
-    seed.iter().for_each(|f| fish[*f] += 1);
+    seed.iter().for_each(|&state| fish[state] += 1);
 
     let mut day = 0;
+    // each day the fish in one state shift right to the next state,
+    // spawning new fish when state is 0
     while day < days {
-        let tmp = fish[0];
+        let will_spawn = fish[0];
         for i in 0..fish.len() {
             if i < 8 {
                 fish[i] = fish[i + 1];
             }
         }
-        fish[8] = tmp;
-        fish[6] += tmp;
+        // fish with state 0 create a new fish with state 8
+        fish[8] = will_spawn;
+        // fish with state 0 becomes a fish with state 6
+        fish[6] += will_spawn;
         day += 1;
     }
 
     fish.iter().sum()
-}
-
-fn main() {
-    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    println!(
-        "Total fish after 80 days: \n{:?}",
-        simulate_lanternfish(parse_input(&contents), 80)
-    );
-    println!(
-        "Total fish after 256 days: \n{:?}",
-        simulate_lanternfish(parse_input(&contents), 256)
-    );
 }
