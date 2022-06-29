755eb2a9e17fa9b1c154b2c6f453e008592355f4
oddgrd <oddgrd@gmail.com>
2022-06-29 17:22:29 +0200

    Refactor

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,6 +1,20 @@
-use std::collections::HashMap;
-use std::fs;
-use std::time::Instant;
+use std::{collections::HashMap, fs, time::Instant};
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+
+    let adjacency_list = build_adjacency_list(parse_input(&contents));
+
+    let now = Instant::now();
+    let part_one = find_paths(&adjacency_list, "start", &[], true);
+    let part_two = find_paths(&adjacency_list, "start", &[], false);
+    let time = now.elapsed().as_micros(); // 25ms
+
+    println!(
+        "Part one: {}\nPart two: {}\nTime: {} μs",
+        part_one, part_two, time
+    );
+}
 
 fn parse_input(input: &str) -> Vec<(&str, &str)> {
     input.lines().fold(Vec::new(), |mut edges, line| {
@@ -9,7 +23,7 @@ fn parse_input(input: &str) -> Vec<(&str, &str)> {
     })
 }
 
-fn build_adj_list<'a>(edges: Vec<(&'a str, &'a str)>) -> HashMap<&'a str, Vec<&'a str>> {
+fn build_adjacency_list<'a>(edges: Vec<(&'a str, &'a str)>) -> HashMap<&'a str, Vec<&'a str>> {
     edges.iter().fold(HashMap::new(), |mut adj_list, (a, b)| {
         adj_list.entry(a).or_insert_with(Vec::new).push(b);
         adj_list.entry(b).or_insert_with(Vec::new).push(a);
@@ -17,13 +31,13 @@ fn build_adj_list<'a>(edges: Vec<(&'a str, &'a str)>) -> HashMap<&'a str, Vec<&'
     })
 }
 
-fn is_lower(s: &str) -> bool {
-    s == s.to_lowercase()
+fn is_lower(s: u8) -> bool {
+    ((s >> 5) & 1) != 0
 }
 
 fn contains_duplicate_small(path: &[&str]) -> bool {
     let mut visited = Vec::new();
-    for s in path.iter().filter(|n| is_lower(n)).collect::<Vec<&&str>>() {
+    for s in path.iter().filter(|n| is_lower(n.as_bytes()[0])) {
         if visited.contains(&s) {
             return true;
         }
@@ -38,45 +52,28 @@ fn find_paths(
     visited: &[&str],
     visited_small: bool,
 ) -> i32 {
-    let mut path_count = 0;
-    let mut path = visited.to_vec();
-    path.push(cave);
-
     if cave == "end" {
         return 1;
     }
+    let mut path_count = 0;
+    let path: &[&str] = &[visited, &[cave]].concat();
 
-    let contains_dup = if !visited_small {
-        contains_duplicate_small(&path)
+    let contains_duplicate = if !visited_small {
+        contains_duplicate_small(path)
     } else {
         visited_small
     };
 
     let edges = graph.get(cave).unwrap();
     edges.iter().for_each(|e| {
-        if *e != "start" && (!is_lower(e) || (!contains_dup || !path.contains(e))) {
-            path_count += find_paths(graph, e, &path, contains_dup);
+        if *e != "start"
+            && (!is_lower(e.as_bytes()[0]) || (!contains_duplicate || !path.contains(e)))
+        {
+            path_count += find_paths(graph, e, path, contains_duplicate);
         }
     });
 
     path_count
-}
-
-fn main() {
-    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    let adj_list = build_adj_list(parse_input(&contents));
-
-    let now = Instant::now();
-    println!(
-        "Part one paths: {:?}",
-        find_paths(&adj_list, "start", &[], true)
-    );
-    println!(
-        "Part two paths: {:?}",
-        find_paths(&adj_list, "start", &[], false)
-    );
-
-    println!("Timer: {}ms", now.elapsed().as_millis()); // 0.8s
 }
 
 #[cfg(test)]
@@ -97,7 +94,7 @@ kj-sa
 kj-HN
 kj-dc";
 
-        let adj_list = build_adj_list(parse_input(input));
+        let adj_list = build_adjacency_list(parse_input(input));
         assert_eq!(find_paths(&adj_list, "start", &[], true), 19);
     }
 
@@ -112,7 +109,7 @@ b-d
 A-end
 b-end";
 
-        let adj_list = build_adj_list(parse_input(input));
+        let adj_list = build_adjacency_list(parse_input(input));
         assert_eq!(find_paths(&adj_list, "start", &[], false), 36);
     }
 }
